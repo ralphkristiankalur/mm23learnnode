e98ef82cdab6566b0a483d8a3c3ad7edf266b604
Ralph Kristian Kalur <ralphkristian.kalur@tptlive.ee>
2025-01-27 00:00:34 +0200

    Add bootstrap, start copying wp theme html

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
-import './style.css';
+import './style.scss';
+import * as bootstrap from 'bootstrap'
 console.log('hello webpack')
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
 module. exports = {
     entry: './src/index.js',
@@ -21,6 +22,39 @@ module. exports = {
                 test:/\.css$/i,
                 use: ["style-loader", "css-loader"],
             },
+            {
+                test: /\.(scss)$/,
+                use: [
+                  {
+                    // Adds CSS to the DOM by injecting a `<style>` tag
+                    loader: 'style-loader'
+                  },
+                  {
+                    // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                    loader: 'css-loader'
+                  },
+                  {
+                    // Loader for webpack to process CSS with PostCSS
+                    loader: 'postcss-loader',
+                    options: {
+                      postcssOptions: {
+                        plugins: [
+                          autoprefixer
+                        ]
+                      }
+                    }
+                  },
+                  {
+                    // Loads a SASS/SCSS file and compiles it to CSS
+                    loader: 'sass-loader',
+                    options: {
+                        sassOptions: {
+                            quietDeps: true
+                        }
+                    }
+                  }
+                ]
+              }
         ],
     },
     plugins: [

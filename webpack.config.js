4c42acbdd4fcab99beb4dae6d739656a267c5d73
Ralph Kristian Kalur <ralphkristian.kalur@tptlive.ee>
2025-02-10 22:50:37 +0200

    Add nunjucks, translate undex to use includes

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,77 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const autoprefixer = require('autoprefixer');
+const loader = require('sass-loader');
 
-module. exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
     },
-    devServer: {
-        static:{
-            directory: path.join(__dirname, 'public'),
-        },
-        compress: true,
-        port: 9000,
-    },
-    module: {
-        rules: [
-            {
-                test:/\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.(scss)$/,
-                use: [
-                  {
-                    // Adds CSS to the DOM by injecting a `<style>` tag
-                    loader: 'style-loader'
-                  },
-                  {
-                    // Interprets `@import` and `url()` like `import/require()` and will resolve them
-                    loader: 'css-loader'
-                  },
-                  {
-                    // Loader for webpack to process CSS with PostCSS
-                    loader: 'postcss-loader',
-                    options: {
-                      postcssOptions: {
-                        plugins: [
-                          autoprefixer
-                        ]
-                      }
-                    }
-                  },
-                  {
-                    // Loads a SASS/SCSS file and compiles it to CSS
-                    loader: 'sass-loader',
-                    options: {
-                        sassOptions: {
-                            quietDeps: true
-                        }
-                    }
-                  }
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: 'style-loader'
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader'
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  autoprefixer
                 ]
               }
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
+            }
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                quietDeps: true
+              }
+            }
+          }
+        ]
+      },
+      { 
+        test: /\.njk$/,
+        use: [
+          {
+            loader: 'simple-nunjucks-loader',
+            options: {}
+          }
+        ]
+
+
+      }
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.njk'
+    })
+  ],
 };

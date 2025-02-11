5643cde0125879198ac0ddd292d442882003f892
Ralph Kristian Kalur <ralphkristian.kalur@tptlive.ee>
2025-02-11 22:25:33 +0200

    Change webpack to use imports, add fetch data example

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const autoprefixer = require('autoprefixer');
-const loader = require('sass-loader');
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import autoprefixer from 'autoprefixer';
+import { fileURLToPath } from 'url';
 
-module.exports = {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname =path.dirname(__filename);
+
+
+let response = await fetch('https://api.spaceflightnewsapi.net/v4/articles/?format=json&limit=12');
+let body = await response.json();
+let articles = body.results;
+
+export default {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
@@ -80,6 +88,9 @@ module.exports = {
     new HtmlWebpackPlugin({
       filename: 'blog.html',
       template: './src/blog.njk',
+      templateParameters: {
+        articles: articles
+      }
     }),
     new HtmlWebpackPlugin({
       filename: 'contact.html',

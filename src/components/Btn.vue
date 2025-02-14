2b9bccfb18525cf63afa3d9b150be37862538834
Ralph Kristian Kalur <ralphkristian.kalur@tptlive.ee>
2025-02-14 20:16:04 +0200

    Add button component

---
+++ src/components/Btn.vue
@@ -0,0 +1,46 @@
+
+<script setup>
+import { computed } from 'vue';
+let props = defineProps({
+    color: {
+        type: String,
+        required: false,
+        validator(value, props) {
+            return [
+                'primary',
+                'link',
+                'info',
+                'success',
+                'warning',
+                'danger',
+                'white',
+                'light',
+                'dark',
+                'text',
+                'ghost',
+            ].includes(value);
+        }
+    },
+    light: {
+        type: Boolean,
+        required: false,
+    }    
+});
+
+let classNames = computed(() => {
+    let names = [];
+    if(props.color){
+        names.push('is-' + props.color);
+    }
+    if(props.light){
+        names.push('is-light');
+    }
+    console.log(props);
+    return names.join(' ');
+})
+</script>
+<template>
+    <button class="button" :class="classNames">
+        <slot></slot>
+    </button>
+</template>

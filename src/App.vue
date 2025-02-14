23536c4e729268c1e542f1c8e4033bb66a4257a5
Ralph Kristian Kalur <ralphkristian.kalur@tptlive.ee>
2025-02-14 20:18:23 +0200

    Finish button component

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import Btn from './components/Btn.vue';
 </script>
 <template>
     <div class="container mt-2">
-        <Btn color="primary" light>Button is cool</Btn>
+        <Btn color="primary" dark size="large" rounded state="loading">Button is cool</Btn>
+        
     </div>
 </template>
--- src/components/Btn.vue
+++ src/components/Btn.vue
@@ -24,7 +24,60 @@ let props = defineProps({
     light: {
         type: Boolean,
         required: false,
-    }    
+    },
+    dark: {
+        type: Boolean,
+        required: false,
+    },
+    size: {
+        type: String,
+        required: false,
+        validator(value, props) {
+            return [
+                'small',
+                'normal',
+                'medium',
+                'large',
+            ].includes(value);
+        }
+    },
+    responsive: {
+        type: Boolean,
+        required: false,
+    },
+    fullwidth: {
+        type: Boolean,
+        required: false,
+    },
+    outlined: {
+        type: Boolean,
+        required: false,
+    },
+    inverted: {
+        type: Boolean,
+        required: false,
+    },
+    rounded: {
+        type: Boolean,
+        required: false,
+    },
+    state: {
+        type: String,
+        required: false,
+        validator(value, props) {
+            return [
+                'hover',
+                'focus',
+                'active',
+                'loading',
+                'static'
+            ].includes(value);
+        }
+    },
+    disabled: {
+        type: Boolean,
+        required: false,
+    },
 });
 
 let classNames = computed(() => {
@@ -34,6 +87,33 @@ let classNames = computed(() => {
     }
     if(props.light){
         names.push('is-light');
+    }
+    if(props.dark){
+        names.push('is-dark');
+    }
+    if(props.size){
+        names.push('is-' + props.size);
+    }
+    if(props.responsive){
+        names.push('is-responsive');
+    }
+    if(props.responsive){
+        names.push('is-fullwidth');
+    }
+    if(props.outlined){
+        names.push('is-outlined');
+    }
+    if(props.inverted){
+        names.push('is-inverted');
+    }
+    if(props.rounded){
+        names.push('is-rounded');
+    }
+    if(props.state){
+        names.push('is-' + props.state);
+    }
+    if(props.disabled){
+        names.push('is-disabled');
     }
     console.log(props);
     return names.join(' ');
